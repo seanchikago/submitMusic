e4a47ed033585bcddc11c71d3dc20289fb32e5e7
seanchikago <34871450+seanchikago@users.noreply.github.com>
2017-12-27 01:25:03 +0100

    My SM Checkin
    Fiirs SM CHeckin

---
+++ username-checker.php
@@ -0,0 +1,27 @@
+<?php
+require ("db.php");
+
+ if(isset($_POST["username"]))
+{
+    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+        die();
+    }
+    $mysqli = new mysqli('host' , 'sql_username', 'sql_pass', 'database');
+    if ($mysqli->connect_error){
+        die('Could not connect to database!');
+    }
+    
+    $username = filter_var($_POST["username"], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH);
+    
+    $statement = $mysqli->prepare("SELECT username FROM user_list WHERE username=?");
+    $statement->bind_param('s', $username);
+    $statement->execute();
+    $statement->bind_result($username);
+    if($statement->fetch()){
+        die('<img src="public/images/not-available.png" />');
+    }else{
+        die('<img src="public/images/available.png" />');
+    }
+}
+
+?>
